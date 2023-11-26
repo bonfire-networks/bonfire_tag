b4357a20ea0a7701c9a3fc2200abdd4538c4039c
Mayel de Borniol <git@mayel.space>
2023-11-26 18:42:46 +0000

    f

--- lib/tagged.ex
+++ lib/tagged.ex
@@ -62,7 +62,7 @@ defmodule Bonfire.Tag.Tagged do
         [tag: [:profile, :geolocation, :category, :character]]
       ])
 
-    Map.put(tagged, :thing, Bonfire.Common.Pointers.get(tagged.pointer))
+    Map.put(tagged, :thing, Bonfire.Common.Pointers.get(Utils.e(tagged, :pointer, nil)))
   end
 
   @doc """
