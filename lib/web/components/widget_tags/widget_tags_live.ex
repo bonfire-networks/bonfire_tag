273339cb177707c90d7af6040f61b20611532b5b
Mayel de Borniol <git@mayel.space>
2024-03-11 13:40:55 +0000

    https://github.com/bonfire-networks/bonfire-app/issues/860

--- lib/web/components/widget_tags/widget_tags_live.ex
+++ lib/web/components/widget_tags/widget_tags_live.ex
@@ -2,4 +2,5 @@ defmodule Bonfire.Tag.Web.WidgetTagsLive do
   use Bonfire.UI.Common.Web, :stateless_component
 
   prop widget_title, :string, default: nil
+  prop wrapper_class, :string, default: nil
 end
