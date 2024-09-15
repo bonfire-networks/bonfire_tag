6af645c6f2a0bae0ec542e4b282710b2f265f4d4
chobeat <simone@robutti.me>
2024-09-15 10:33:26 +0200

    added basic post tagging test

---
+++ test/post_tagging_test.exs
@@ -0,0 +1,44 @@
+defmodule Bonfire.Tag.PostsTest do
+  use Bonfire.Tag.DataCase, async: true
+  use Bonfire.Common.Utils
+
+  alias Bonfire.Common.Config
+  alias Bonfire.Me.Fake
+  import Bonfire.Posts.Fake
+  use Bonfire.Common.Utils
+
+  def repo, do: Config.repo()
+
+  test "post without hashtags contains no tags" do
+    user = Fake.fake_user!()
+
+    post =
+      fake_post!(user, "public", %{
+        post_content: %{
+          summary: "summary",
+          name: "name",
+          html_body: "epic html"
+        }
+      })
+      |> repo().maybe_preload(tagged: :tag)
+
+    assert post.tagged == []
+  end
+
+  test "post gets tagged at creation" do
+    user = Fake.fake_user!()
+
+    post =
+      fake_post!(user, "public", %{
+        post_content: %{
+          summary: "summary",
+          name: "name",
+          html_body: "epic html #my_tag"
+        }
+      })
+      |> repo().maybe_preload(tagged: [tag: :named])
+
+    assert [%{tag: %{named: %{name: "my_tag"}}}] = post.tagged
+    assert post.post_content.html_body =~ "[#my_tag](/hashtag/my_tag)"
+  end
+end
