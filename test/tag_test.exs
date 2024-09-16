b91f33a0b52d447a7a3c6190b7d9f2e37c7b971a
chobeat <simone@robutti.me>
2024-09-16 18:52:46 +0200

    added Tag.cast test

--- test/tag_test.exs
+++ test/tag_test.exs
@@ -2,10 +2,11 @@ defmodule Bonfire.Tag.TagTest do
   use Bonfire.Tag.DataCase, async: true
   use Bonfire.Common.Utils
   import Bonfire.Posts.Fake
+  alias Bonfire.Posts
   alias Bonfire.Tag
-  alias Bonfire.Me.Fake
   alias Needle.Tables
-
+  alias Ecto.Changeset
+  alias Bonfire.Me.Fake
   def repo, do: Config.repo()
 
   test "tag creation" do
@@ -15,5 +16,17 @@ defmodule Bonfire.Tag.TagTest do
 
     {:ok, tag_2} = Tag.get_or_create_hashtag("test")
     assert tag_1.id == tag_2.id
+  end
+
+  test "Tag.cast should add a Tagged entry to the changeset" do
+    user = Fake.fake_user!()
+
+    {:ok, %{id: tag_id} = tag} = Tag.get_or_create_hashtag("test")
+
+    changeset =
+      Tag.cast(Posts.changeset(:create, %{}), %{tags: [tag]}, user, put_tree_parent: false)
+
+    assert [%{action: :insert, valid?: true, params: %{"tag_id" => tag_id}}] =
+             changeset.changes.tagged
   end
 end
