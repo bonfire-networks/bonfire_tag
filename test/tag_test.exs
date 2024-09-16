81f3b30c735c065fde183bb82b81b1e89d6edd17
chobeat <simone@robutti.me>
2024-09-16 15:52:06 +0200

    added tag creation test

--- test/tag_test.exs
+++ test/tag_test.exs
@@ -1,13 +1,19 @@
 defmodule Bonfire.Tag.TagTest do
   use Bonfire.Tag.DataCase, async: true
   use Bonfire.Common.Utils
+  import Bonfire.Posts.Fake
   alias Bonfire.Tag
+  alias Bonfire.Me.Fake
+  alias Needle.Tables
 
   def repo, do: Config.repo()
 
-  test "hashtag creation" do
+  test "tag creation" do
     assert {:error, _} = Tag.get("test")
-    {:ok, tag} = Tag.get_or_create_hashtag("test")
-    assert tag.named.name == "test"
+    {:ok, tag_1} = Tag.get_or_create_hashtag("test")
+    assert tag_1.named.name == "test"
+
+    {:ok, tag_2} = Tag.get_or_create_hashtag("test")
+    assert tag_1.id == tag_2.id
   end
 end
