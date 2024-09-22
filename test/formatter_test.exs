046c929af87af7cd95a33e846bad46e6e2ce596a
chobeat <simone@robutti.me>
2024-09-22 14:27:46 +0200

    parametrized tests

--- test/formatter_test.exs
+++ test/formatter_test.exs
@@ -8,30 +8,34 @@ defmodule Bonfire.Tag.PostsTest do
       assert {"", [], [], []} = Formatter.linkify("")
     end
 
-    test "find mention if the user exists and linkify it" do
-      user = Fake.fake_user!()
-      username = user.character.username
-      content_with_mention = "hi @#{username}"
+    for prefix <- ["@", "+", "&"] do
+      test "using #{prefix}, find mention if the user exists and linkify it" do
+        user = Fake.fake_user!()
+        username = user.character.username
+        content_with_mention = "hi #{unquote(prefix)}#{username}"
 
-      assert {linkified_content, [{returned_display_name, %{id: returned_user_id}}], [], []} =
-               Formatter.linkify(content_with_mention,
-                 safe_mention: false,
-                 content_type: "text/markdown"
-               )
+        assert {linkified_content, [{returned_display_name, %{id: returned_user_id}}], [], []} =
+                 Formatter.linkify(content_with_mention,
+                   safe_mention: false,
+                   content_type: "text/markdown"
+                 )
 
-      assert linkified_content =~ "hi [#{returned_display_name}]("
-      assert returned_display_name == "@#{username}"
-      assert returned_user_id == user.id
-    end
+        assert linkified_content =~ "hi [#{returned_display_name}]("
+        assert returned_display_name == "#{unquote(prefix)}#{username}"
+        assert returned_user_id == user.id
+      end
 
-    test "find no mention if the user doesn't exist" do
-      content_with_mention = "hi @missing_user"
+      test "using #{prefix}, find no mention if the user doesn't exist" do
+        content_with_mention = "hi #{unquote(prefix)}missing_user"
 
-      assert {content_with_mention, [], [], []} =
-               Formatter.linkify(content_with_mention,
-                 safe_mention: false,
-                 content_type: "text/markdown"
-               )
+        assert {found_content, [], [], []} =
+                 Formatter.linkify(content_with_mention,
+                   safe_mention: false,
+                   content_type: "text/markdown"
+                 )
+
+        assert found_content == content_with_mention
+      end
     end
   end
 end
