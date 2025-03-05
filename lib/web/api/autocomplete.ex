260c0ad7f57f71ccb60d0b093aea8da1d2e4ffb2
ivan <ivanminutillo@outlook.it>
2025-03-05 21:14:53 +0000

    wip

--- lib/web/api/autocomplete.ex
+++ lib/web/api/autocomplete.ex
@@ -9,13 +9,11 @@ defmodule Bonfire.Tag.Web.API.Autocomplete do
         "consumer" => consumer
       }) do
     tags = Autocomplete.api_tag_lookup(search, prefix, consumer)
-
     json(conn, prepare(tags))
   end
 
   def get(conn, %{"prefix" => prefix, "search" => search}) do
     tags = Autocomplete.api_tag_lookup(search, prefix, "tag_as")
-
     json(conn, prepare(tags))
   end
 
