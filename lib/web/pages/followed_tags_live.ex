8ead699831ca7ceed2b1a509fda302d947c12cc6
ivan <ivanminutillo@outlook.it>
2025-12-15 21:48:44 +0100

    pagination

--- lib/web/pages/followed_tags_live.ex
+++ lib/web/pages/followed_tags_live.ex
@@ -7,23 +7,45 @@ defmodule Bonfire.Tag.Web.FollowedTagsLive do
   def mount(_params, _session, socket) do
     current_user = current_user(socket.assigns)
 
-    followed_tags =
-      if current_user do
-        Follows.list_followed(current_user, type: Bonfire.Tag.Hashtag)
-        |> e(:edges, [])
-        |> Enum.map(&e(&1, :edge, :object, nil))
-        |> Enum.reject(&is_nil/1)
-        |> repo().maybe_preload(:named)
-      else
-        []
-      end
+    {followed_tags, page_info} = list_followed_tags(current_user, %{})
 
     {:ok,
      assign(socket,
        page_title: l("Followed Hashtags"),
        followed_tags: followed_tags,
+       page_info: page_info,
        back: true,
        nav_items: Bonfire.Common.ExtensionModule.default_nav()
      )}
+  end
+
+  def handle_event("load_more", attrs, socket) do
+    current_user = current_user(socket.assigns)
+    {new_tags, page_info} = list_followed_tags(current_user, attrs)
+
+    {:noreply,
+     assign(socket,
+       followed_tags: e(socket.assigns, :followed_tags, []) ++ new_tags,
+       page_info: page_info
+     )}
+  end
+
+  defp list_followed_tags(nil, _attrs), do: {[], nil}
+
+  defp list_followed_tags(current_user, attrs) do
+    result =
+      Follows.list_followed(current_user,
+        type: Bonfire.Tag.Hashtag,
+        paginate: input_to_atoms(attrs)
+      )
+
+    tags =
+      result
+      |> e(:edges, [])
+      |> Enum.map(&e(&1, :edge, :object, nil))
+      |> Enum.reject(&is_nil/1)
+      |> repo().maybe_preload(:named)
+
+    {tags, e(result, :page_info, nil)}
   end
 end
