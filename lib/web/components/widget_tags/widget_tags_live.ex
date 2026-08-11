a7121d937650fbfc53b41de6668f2e0db68b3d07
Mayel de Borniol <git@mayel.space>
2026-08-11 19:39:22 +0100

    localise

--- lib/web/components/widget_tags/widget_tags_live.ex
+++ lib/web/components/widget_tags/widget_tags_live.ex
@@ -17,7 +17,8 @@ defmodule Bonfire.Tag.Web.WidgetTagsLive do
      #  TODO: how to update them without reloading or making this a stateful component
      |> assign_flash(
        :info,
-       l("Trending tags have been reset.") <> l(" You need to reload to see updates, if any.")
+       l("Trending tags have been reset.") <>
+         " " <> l("You need to reload to see updates, if any.")
      )}
   end
 end
